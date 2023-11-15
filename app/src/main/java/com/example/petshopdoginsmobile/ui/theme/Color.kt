b7535421a098978dbcf2970a92baab45a8aed842
aasjunior <adeldivo@hotmail.com>
2023-11-15 15:54:51 -0300

    style(ui): add fonts and colors

--- app/src/main/java/com/example/petshopdoginsmobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/petshopdoginsmobile/ui/theme/Color.kt
@@ -2,6 +2,18 @@ package com.example.petshopdoginsmobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Red = Color(0xFFFF0000)
+val Blue = Color(0xFF85D3FF)
+val BlueDark = Color(0xFF2863BE)
+val SoftBlue = Color(0xFFCDEEFF)
+val VibrantBlue = Color(0xFF4B8CEF)
+val Grey = Color(0xFFC4C4C4)
+val GreyDarkier = Color(0xFF797979)
+val BgGrey = Color(0xFFF1F1F1)
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
---
+++ app/src/main/java/com/example/petshopdoginsmobile/ui/theme/Text.kt
@@ -0,0 +1,31 @@
+package com.example.petshopdoginsmobile.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.petshopdoginsmobile.R
+
+
+data class CustomTextStyles(
+    val h1: TextStyle,
+    val h2: TextStyle,
+    val h3: TextStyle,
+    val h4: TextStyle,
+    val body1: TextStyle,
+    val body2: TextStyle,
+    val body3: TextStyle,
+    val link1: TextStyle,
+    val button1: TextStyle,
+    val button2: TextStyle,
+    val overline1: TextStyle
+
+)
+
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+)
+
