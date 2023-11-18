3db2d49fd4d6d92ecc473401dcc2f6904df735f2
Isabelle Hondo <isefshondo@gmail.com>
2023-11-18 12:26:07 -0300

    feat: visualize product card structure

--- app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/VisualizeProductCard.kt
+++ app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/VisualizeProductCard.kt
@@ -1,6 +1,8 @@
 package com.example.petshopdoginsmobile.ui.components.cards
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,11 +12,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import com.example.petshopdoginsmobile.R
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -72,9 +78,15 @@ fun VisualizeProductCard() {
                         modifier = Modifier
                             .weight(3F)
                             .fillMaxHeight()
-                            .background(Color.Red)
+                            .background(Color.Red),
+                        contentAlignment = Alignment.Center,
                     ) {
-
+                        Image(
+                            painter = painterResource(id = R.drawable.img_cat),
+                            contentDescription = "Image of product",
+                            modifier = Modifier
+                                .fillMaxSize(),
+                        )
                     }
                     Box (
                         modifier = Modifier
@@ -90,6 +102,20 @@ fun VisualizeProductCard() {
                             IconButton(image = R.drawable.share_icon, onClick = {})
                         }
                     }
+                }
+            }
+            Box (
+                modifier = Modifier
+                    .padding(vertical = 15.dp)
+                    .fillMaxWidth(),
+            ) {
+                Box (
+                    modifier = Modifier
+                        .border(width = 2.dp, color = Blue, shape = RoundedCornerShape(10.dp))
+                        .clip(RoundedCornerShape(10.dp))
+                        .size(50.dp)
+                ) {
+
                 }
             }
         }
