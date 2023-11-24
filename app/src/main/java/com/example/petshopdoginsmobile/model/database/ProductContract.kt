3c2a66786afff9aea755ade31d960f5e3cd362e2
mandis-ncs <amnd.castro@gmail.com>
2023-11-23 22:15:22 -0300

    updating sqlite contract and helper

--- app/src/main/java/com/example/petshopdoginsmobile/model/database/ProductContract.kt
+++ app/src/main/java/com/example/petshopdoginsmobile/model/database/ProductContract.kt
@@ -3,10 +3,24 @@ package com.example.petshopdoginsmobile.model.database
 import android.provider.BaseColumns
 
 object ProductContract {
+    object ProductImageEntry : BaseColumns {
+        const val TABLE_NAME = "product_image"
+        const val COLUMN_NAME_TYPE = "type"
+        const val COLUMN_NAME_DATA = "data"
+    }
+
     object ProductEntry : BaseColumns {
         const val TABLE_NAME = "product"
         const val COLUMN_NAME_ID = "id"
         const val COLUMN_NAME_CATEGORY = "category"
-        // Adicione todas as outras colunas aqui
+        const val COLUMN_NAME_COLOR = "color"
+        const val COLUMN_NAME_DESCRIPTION = "description"
+        const val COLUMN_NAME_IMAGES = "images" // This will be a foreign key
+        const val COLUMN_NAME_BRAND = "brand"
+        const val COLUMN_NAME_PRICE = "price"
+        const val COLUMN_NAME_STOCK = "stock"
+        const val COLUMN_NAME_SIZE = "size"
+        const val COLUMN_NAME_NAME = "name"
     }
+
 }
--- app/src/main/java/com/example/petshopdoginsmobile/model/database/ProductDbHelper.kt
+++ app/src/main/java/com/example/petshopdoginsmobile/model/database/ProductDbHelper.kt
@@ -8,23 +8,45 @@ import android.provider.BaseColumns
 // methods to modifier db goes here
 class ProductDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(SQL_CREATE_ENTRIES)
+        db.execSQL(SQL_CREATE_ENTRIES_PRODUCT_IMAGE)
+        db.execSQL(SQL_CREATE_ENTRIES_PRODUCT)
     }
 
+
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        db.execSQL(SQL_DELETE_ENTRIES)
+        db.execSQL(SQL_DELETE_ENTRIES_PRODUCT_ENTRY)
+        db.execSQL(SQL_DELETE_ENTRIES_PRODUCT_IMAGE)
         onCreate(db)
     }
 
     companion object {
         const val DATABASE_NAME = "Products.db"
         const val DATABASE_VERSION = 1
-        const val SQL_CREATE_ENTRIES = "CREATE TABLE ${ProductContract.ProductEntry.TABLE_NAME} (" +
-                "${BaseColumns._ID} INTEGER PRIMARY KEY," +
-                "${ProductContract.ProductEntry.COLUMN_NAME_ID} TEXT," +
-                "${ProductContract.ProductEntry.COLUMN_NAME_CATEGORY} TEXT" +
-                // Adicione todas as outras colunas aqui
-                ")"
-        const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${ProductContract.ProductEntry.TABLE_NAME}"
+        const val SQL_CREATE_ENTRIES_PRODUCT_IMAGE =
+            "CREATE TABLE ${ProductContract.ProductImageEntry.TABLE_NAME} (" +
+                    "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                    "${ProductContract.ProductImageEntry.COLUMN_NAME_TYPE} INTEGER," +
+                    "${ProductContract.ProductImageEntry.COLUMN_NAME_DATA} TEXT)"
+
+        const val SQL_CREATE_ENTRIES_PRODUCT =
+            "CREATE TABLE ${ProductContract.ProductEntry.TABLE_NAME} (" +
+                    "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_ID} TEXT," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_CATEGORY} TEXT," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_COLOR} TEXT," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_DESCRIPTION} TEXT," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_IMAGES} INTEGER," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_BRAND} TEXT," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_PRICE} REAL," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_STOCK} INTEGER," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_SIZE} TEXT," +
+                    "${ProductContract.ProductEntry.COLUMN_NAME_NAME} TEXT," +
+                    "FOREIGN KEY(${ProductContract.ProductEntry.COLUMN_NAME_IMAGES}) REFERENCES ${ProductContract.ProductImageEntry.TABLE_NAME}(${BaseColumns._ID}))"
+
+
+        const val SQL_DELETE_ENTRIES_PRODUCT_ENTRY =
+            "DROP TABLE IF EXISTS ${ProductContract.ProductEntry.TABLE_NAME}"
+        const val SQL_DELETE_ENTRIES_PRODUCT_IMAGE=
+            "DROP TABLE IF EXISTS ${ProductContract.ProductImageEntry.TABLE_NAME}"
     }
 }
