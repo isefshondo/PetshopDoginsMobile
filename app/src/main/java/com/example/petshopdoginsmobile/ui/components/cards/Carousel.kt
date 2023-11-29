0a9a3d240971ee2632c38a8ed8a58a197a92dde9
aasjunior <adeldivo@hotmail.com>
2023-11-28 22:19:06 -0300

    feat(ui): add carousel HomePage

--- app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/Carousel.kt
+++ app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/Carousel.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +14,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.petshopdoginsmobile.R
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.platform.LocalConfiguration
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -21,18 +29,24 @@ fun Carousel(
     items: List<Int>,
     modifier: Modifier = Modifier
 ) {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
     val pagerState = rememberPagerState()
+
     Box(modifier = Modifier.fillMaxWidth()){
         HorizontalPager(
             pageCount = items.size,
             state = pagerState,
-            key = { items[it] }
+            key = { items[it] },
+            pageSize = PageSize.Fixed(screenWidth * 0.9f)
         ) { page ->
             Image(
                 painter = painterResource(id = items[page]),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxWidth(0.9f)
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .offset(x = 20.dp)
             )
         }
     }
