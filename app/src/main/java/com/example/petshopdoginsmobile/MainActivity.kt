c2fa0d90c2055e548d8c1dc124566df9870a5308
aasjunior <adeldivo@hotmail.com>
2023-11-30 20:56:21 -0300

    feat(ui): add carousel card

--- app/src/main/java/com/example/petshopdoginsmobile/MainActivity.kt
+++ app/src/main/java/com/example/petshopdoginsmobile/MainActivity.kt
@@ -6,12 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.petshopdoginsmobile.ui.components.cards.Carousel
-import com.example.petshopdoginsmobile.ui.pages.ShoppingCartPage
+import com.example.petshopdoginsmobile.ui.components.cards.CarouselCard
 import com.example.petshopdoginsmobile.ui.theme.PetshopDoginsMobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Carousel(items = listOf(
+                    CarouselCard(items = listOf(
                         R.drawable.img_carousel_01,
                         R.drawable.img_carousel_02
                     ))
--- app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/CarouselCard.kt
+++ app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/CarouselCard.kt
@@ -2,30 +2,29 @@ package com.example.petshopdoginsmobile.ui.components.cards
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.petshopdoginsmobile.R
-import android.content.res.Configuration
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Carousel(
+fun CarouselCard(
     items: List<Int>,
     modifier: Modifier = Modifier
 ) {
@@ -37,17 +36,23 @@ fun Carousel(
         HorizontalPager(
             pageCount = items.size,
             state = pagerState,
+            contentPadding = PaddingValues(horizontal = 15.dp),
             key = { items[it] },
             pageSize = PageSize.Fixed(screenWidth * 0.9f)
         ) { page ->
-            Image(
-                painter = painterResource(id = items[page]),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
+            Card(
+                shape = RoundedCornerShape(10.dp),
                 modifier = Modifier
-                    .fillMaxWidth(0.95f)
-                    .offset(x = 20.dp)
-            )
+                    .padding(horizontal = 10.dp)
+            ){
+                Image(
+                    painter = painterResource(id = items[page]),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
         }
     }
 }
@@ -56,8 +61,8 @@ fun Carousel(
 
 @Preview
 @Composable
-private fun CarouselPreview(){
-    Carousel(items = listOf(
+private fun CarouselCardPreview(){
+    CarouselCard(items = listOf(
         R.drawable.img_carousel_01,
         R.drawable.img_carousel_02
     ))
