d2521d925538088d28a6642abbd809b275afec8c
aasjunior <adeldivo@hotmail.com>
2023-12-02 17:35:11 -0300

    feat(ui): add product catalogue

--- app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/ProductCatalogue.kt
+++ app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/ProductCatalogue.kt
@@ -1,20 +1,16 @@
 package com.example.petshopdoginsmobile.ui.components.cards
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
@@ -23,23 +19,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.petshopdoginsmobile.R
 import com.example.petshopdoginsmobile.domain.Product
 import com.example.petshopdoginsmobile.ui.components.buttons.ClickableLinkText
-import com.example.petshopdoginsmobile.ui.components.lines.VerticalDivider
 import com.example.petshopdoginsmobile.ui.theme.BgGrey
 import com.example.petshopdoginsmobile.ui.theme.Green
 import com.example.petshopdoginsmobile.ui.theme.Grey
 import com.example.petshopdoginsmobile.ui.theme.GreyDarkier
 import com.example.petshopdoginsmobile.ui.theme.VibrantBlue
-import com.example.petshopdoginsmobile.ui.theme.discount
 import com.example.petshopdoginsmobile.ui.theme.medium14
 import com.example.petshopdoginsmobile.ui.theme.regular10
 import com.example.petshopdoginsmobile.ui.theme.regular12
@@ -84,15 +80,28 @@ fun ProductCatalogue(
                         for (columnIndex in 0 until 2) {
                             val index = rowIndex * 2 + columnIndex
                             if (index < products.size) {
-                                Column(
-                                    modifier = Modifier
-                                        .widthIn(min = itemWidth),
-                                    horizontalAlignment = Alignment.CenterHorizontally
-                                ){
-                                    ProductItem(
-                                        product = products[index],
-                                        shippingCost = shippingCost
-                                    )
+                                var itemModifier = Modifier.widthIn(min = itemWidth)
+                                if(columnIndex == 0) {
+                                    itemModifier = itemModifier.drawBehind {
+                                        val strokeWidth = 1.dp.toPx() // Define a espessura da borda
+                                        val color = BgGrey // Define a cor da borda
+                                        drawRect(
+                                            color,
+                                            Offset(size.width - strokeWidth, 0f),
+                                            Size(strokeWidth, size.height)
+                                        )
+                                    }
+                                }
+                                Box{
+                                    Column(
+                                        modifier = itemModifier,
+                                        horizontalAlignment = Alignment.CenterHorizontally
+                                    ){
+                                        ProductItem(
+                                            product = products[index],
+                                            shippingCost = shippingCost
+                                        )
+                                    }
                                 }
                             }
                         }
