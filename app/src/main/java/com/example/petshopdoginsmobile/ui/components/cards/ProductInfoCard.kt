a6ace55f2b721709fb60013e46874d83a3921650
Isabelle Hondo <isefshondo@gmail.com>
2023-12-01 23:31:31 -0300

    feat: shadow in ProductInfoCard

--- app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/ProductInfoCard.kt
+++ app/src/main/java/com/example/petshopdoginsmobile/ui/components/cards/ProductInfoCard.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,15 +16,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -51,6 +47,7 @@ import com.example.petshopdoginsmobile.ui.theme.GreyDarkier
 import com.example.petshopdoginsmobile.ui.theme.PetshopDoginsMobileTheme
 import com.example.petshopdoginsmobile.ui.theme.SoftBlue
 import com.example.petshopdoginsmobile.ui.theme.VibrantBlue
+import com.example.petshopdoginsmobile.ui.theme.White
 import com.example.petshopdoginsmobile.ui.theme.medium12
 import com.example.petshopdoginsmobile.ui.theme.medium14
 import com.example.petshopdoginsmobile.ui.theme.medium20
@@ -195,7 +192,12 @@ fun ProductInfoCard(
 ) {
     Column (
         modifier = Modifier
+            .shadow(
+                elevation = 18.5.dp,
+                shape = RoundedCornerShape(30.dp)
+            )
             .clip(RoundedCornerShape(30.dp))
+            .background(White)
             .padding(23.dp),
     ) {
         Column {
